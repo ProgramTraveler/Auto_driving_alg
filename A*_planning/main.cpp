5a3dd230cbd18b5db48aab4e1e41fc0b1eedfa64
ProgramTraveler <2115655391@qq.com>
2023-08-31 16:25:18 +0800

    2023-08-31

--- A*_planning/main.cpp
+++ A*_planning/main.cpp
@@ -42,22 +42,113 @@ vector<Point> AstarPathPlanning (vector<vector<int>> &gridmap, Point &start, Poi
     vector<Node *> closelist; // 已搜索的节点
 
     openlist.push_back(new Node(start, start.distance(start), start.distance(goal))); // 初始化 将起点添加到 openlist
+    int count = 1; // 统计 new 的次数
+    int count_del = 0; // 统计 delete 的次数
 
     // 进入循环 开始搜索
     // 搜索到终点则返回路径
     vector<Point> path;
     while (!openlist.empty()) {
         // 获取当前搜索的节点 current 既 openlist 中 f 最小的节点
-        sort(openlist.begin(), openlist.end()); // 排序 自定义排序结果
+        sort(openlist.begin(), openlist.end(), NodeCompare{}); // 排序 自定义排序结果
+        Node *current = *openlist.begin(); // 获取 f 最小的点
 
+        // 将 current 对应的点从 opnelist 中删除
+        openlist.erase(openlist.begin());
 
-        
+        // 将 current 添加到 closelist 中
+        closelist.push_back(current);
 
-    };
+        // 对当前搜索节点 current 进行分类讨论 -> 其实就对四个方向进行讨论
+        /*
+            1. current 是终点 则直接返回 path
+            2. current 不是终点 需要讨论其邻近节点 neighbors
+        */
+
+        if (current->point.x == goal.x && current->point.y == goal.y) {
+            while (current != nullptr) { // 利用父节点 从终点向起点回溯最短路径
+                /*
+                    因为起点没有父节点
+                    所以起点的时候 current 的父节点为空
+                */
+                path.push_back(current->point);
+
+                current = current->parent;
+            }
+
+            reverse(path.begin(), path.end()); // 翻转路径
+            // openlist 和 closelist 存储的元素都是 new 动态分配的内存 需要delete 否则会造成内存泄漏
+            for (auto o : openlist) delete o, count_del ++;
+            for (auto c : closelist) delete c, count_del ++;
+            cout << "new count is " << count << " " << "delete count is " << count_del << endl;
+            return path;
+        }
+
+        int x = current->point.x;
+        int y = current->point.y;
+
+        vector<Point> neighbors = { // 八个方位
+            {x - 1, y - 1}, {x - 1, y}, {x + 1, y},
+            {x, y - 1},   {x, y + 1},
+            {x + 1, y - 1},{x + 1, y},{x + 1, y + 1}
+        };
+
+        // 遍历所有方位的邻近节点
+        for (auto n : neighbors) {
+            if ((n.x >= 0 && n.x < row) && (n.y >= 0 && n.y < col) && gridmap[n.x][n.y] == 0) { // 边界和障碍物判断
+                // n 在 closelist 中 表示已经探索过 直接跳过
+                bool inclose = false;
+
+                for (auto c : closelist) {
+                    if (c->point.x == n.x && c->point.y == n.y) {
+                        inclose = true;
+
+                        break;
+                    }
+                }
+
+                if (inclose) continue;
+
+                // n 是否在 openlist 中
+                bool inopen = false;
+                for (auto o : openlist) {
+                    if (o->point.x == n.x && o->point.y == n.y) {
+                        inopen = true;
+
+                        // 对比 f 值 更新代价和父节点 parent
+                        double g = current->g + n.distance(current->point); // g -> 邻近节点 n 到起点的距离 = current 到起点的距离 + current 到邻近节点 n 的距离
+                        double h = n.distance(goal); // 邻近节点到终点的估计代价距离
+                        double f = g + h;
+
+                        if (f < (o->f)) {
+                            o->f = f;
+                            o->parent = current;
+                        }
+
+                        break;
+                    }
+                }
+
+                if (inopen) continue;
+
+                // 不在 openlist 和 closelist 中
+                // 计算代价值 添加到 openlist 中
+                double g = current->g + n.distance(current->point); // g -> 邻近节点 n 到起点的距离 = current 到起点的距离 + current 到邻近节点 n 的距离
+                double h = n.distance(goal); // 邻近节点到终点的估计代价距离
+                double f = g + h;
+                openlist.push_back(new Node(n, g, h, current));
+                count ++;
+
+            } 
+        }
+    }
 
     // 搜索完成没有路径 表示规划失败 返回空路径
+    for (auto o : openlist) delete o, count_del ++;
+    for (auto c : closelist) delete c, count_del ++;
+    cout << "new count is " << count << " " << "delete count is " << count_del << endl;
     return path;
-};
+}
 
 int main () {
     // 定义地图
@@ -75,6 +166,10 @@ int main () {
 
     vector<Point> path = AstarPathPlanning(gridmap, start, goal);
 
+    cout << "the point count is " << path.size() << endl;
+
+    for (auto p : path) cout << "(" << p.x << ", " << p.y << ")" << "-> ";
+    cout << endl;
 
     return 0;
 }
